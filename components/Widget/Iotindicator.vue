9bdc8393aac56b24dd48ac657363afeab081b613
omar rosas <omar.rosas.calderon@gmail.com>
2021-06-25 01:59:23 -0500

    mensajes nuxtjs envio y recibo

--- components/Widget/Iotindicator.vue
+++ components/Widget/Iotindicator.vue
@@ -1,71 +1,73 @@
 <template>
-    <card>
-        <div slot="header">
-          <h4 class="card-title">{{config.selectedDevice.name}}-{{config.variableFullName}}</h4>
-        </div>
-        <i class="fa " :class="[config.icon, getIconColorClass()]" style="font-size:30px" ></i>
-    </card>   
-    
+  <card>
+    <div slot="header">
+      <h4 class="card-title">
+        {{ config.selectedDevice.name }}-{{ config.variableFullName }}
+      </h4>
+    </div>
+    <i                       
+      class="fa "           
+      :class="[config.icon, getIconColorClass()]"
+      style="font-size:30px"
+    ></i>
+  </card>
 </template>
 
 <script>
 export default {
-    //props:['config'],
-    data(){
-        return{
-            value: false,
-            config:{
-                userId: 'userid',
-                selectedDevice:{
-                     name: "Office",
-                     dId: "3458",
-                     templateName: "Power Camera",
-                     templateId: "232gdfgdfgiy232334sdd",
-                     saverRule: true
-                    
-                },
-                variableFullName:"Pump",
-                variable: "uniquestr",
-                icon: "fa-sun",
-                column: 'col-6',
-                widget: 'indicator',
-                class: 'success'
-
-            }
-        };
-    },
-    mounted(){
-        this.$nuxt.$on('widget-topic',this.processReciveData)
-
-    },
-    methods:{
-        processReciveData(data){
-            console.log("received");
-            console.log(data);
-            this.value = data.value;
+  props:['config'],
+  
+  data() {
+    return {
+      value: false
+     /* config: {
+        userId: "userid",
+        selectedDevice: {
+          name: "Office",
+          dId: "3458",
+          templateName: "Power Camera",
+          templateId: "232gdfgdfgiy232334sdd",
+          saverRule: true
         },
+        variableFullName: "Pump",
+        variable: "uniquestr",
+        icon: "fa-sun",
+        column: "col-6",
+        widget: "indicator",
+        class: "success"
+      }*/
+    };
+  },
+  mounted() {
+    //userId/dId/uniquestr/sdata
+    const topic = this.config.userId + "/" + this.config.selectedDevice.dId + "/" + this.config.variable + "/sdata";
+    console.log(topic);
+    this.$nuxt.$on(topic, this.processReceivedData)
+  },
+  methods: {
+    processReceivedData(data) {
+      console.log("received");
+      console.log(data);
+      this.value = data.value;
+    },
 
-        getIconColorClass(){
-            if(!this.value){
-                return "text-dark"
-            }
-            if(this.config.class=="success"){
-                return "text-success"
-            }
-             if(this.config.class=="primary"){
-                return "text-primary"
-            }
-             if(this.config.class=="warning"){
-                return "text-warning"
-            }
-             if(this.config.class=="danger"){
-                return "text-danger"
-            }
-        }
-
+    getIconColorClass() {
+      if (!this.value) {
+        return "text-dark";
+      }
+      if (this.config.class == "success") {
+        return "text-success";
+      }
+      if (this.config.class == "primary") {
+        return "text-primary";
+      }
+      if (this.config.class == "warning") {
+        return "text-warning";
+      }
+      if (this.config.class == "danger") {
+        return "text-danger";
+      }
     }
+  }
 };
-
 </script>
-
-
