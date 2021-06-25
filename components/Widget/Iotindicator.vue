11f96ce357e1661d92c897070ef7aee22130af2e
omar rosas <omar.rosas.calderon@gmail.com>
2021-06-25 00:29:06 -0500

    primer widget indicator

---
+++ components/Widget/Iotindicator.vue
@@ -0,0 +1,39 @@
+<template>
+    <card>
+        <div slot="header">
+          <h4 class="card-title">{{config.selectedDevice.name}}-{{config.variableFullName}}</h4>
+        </div>
+        <i class="fa " :class="[config.icon]" ></i>
+    </card>   
+    
+</template>
+
+<script>
+export default {
+    //props:['config'],
+    data(){
+        return{
+            config:{
+                userId: 'userid',
+                selectedDevice:{
+                     name: "Office",
+                     dId: "3458",
+                     templateName: "Power Camera",
+                     templateId: "232gdfgdfgiy232334sdd",
+                     saverRule: true
+                    
+                },
+                variableFullName:"Pump",
+                variable: "uniquestr",
+                icon: "fa-sun",
+                column: 'col-6',
+                widget: 'indicator',
+                class: 'danger'
+
+            }
+        }
+    }
+}
+</script>
+
+
--- pages/templates.vue
+++ pages/templates.vue
@@ -1,5 +1,5 @@
 <template>
-    <h2>
-        Templates
-    </h2>
+   <div>
+       <iotindicator></iotindicator>
+   </div>
 </template>
